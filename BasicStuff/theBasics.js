af6bab96ded32327df54a19ba61bb6a336887d4c
AymenEssid1 <aymen.essid@esprit.tn>
2023-02-08 17:12:24 +0100

    A QUARTER OF THE WAY

--- BasicStuff/theBasics.js
+++ BasicStuff/theBasics.js
@@ -139,4 +139,149 @@ else{console.log("enjoy your childhood while you can ");}  //else if()
 let online=false;
 if(!online){console.log("you are offline");} //if(online) if u want to check if it's true
 
- */
+
+
+/////////////////////////////////Checked property (radio box )///////////////////
+document.getElementById("checkboxButton").onclick=function ()
+{
+    if(document.getElementById("mycheckbox").checked){ //true or false
+        console.log("yo this is checked ");
+    }
+    else{console.log("cmon man check me !!");}
+
+
+    const radio1=document.getElementById("radio1");
+    const radio2=document.getElementById("radio2");
+    const radio3=document.getElementById("radio3");
+    if(radio1.checked){console.log("you picked 1 yaaay what an achievement ")}
+}
+
+//////////////////////////////////////SWITCH CASE /////////////////////
+let grade="9";
+//switch(){}
+switch (grade)
+{
+    case "A":
+        console.log("excellent");
+        break;
+    case "B":
+        console.log("okayy ")
+        break;
+    default:
+        console.log(grade+"?","if u get anything other than A or B u dumb ")
+
+}
+////////////////////////////////AND  OR  NOT /////////////////////////////////////
+let temp =39;
+if(temp>0 && temp<30){console.log("good weather");} //AND
+if(temp>60 || temp<-50){console.log("bruh u living on mars ?");} //OR
+if(!(temp===420)){console.log("it's not 420");}
+
+/////////////////////////////////WHILE LOOP \\ DO WHILE LOOP//////////////////////////////
+let usernamee ;
+while(usernamee==""){
+    usernamee=window.prompt("enter your name man §§§");
+
+}
+console.log("hello bro ",usernamee);
+//doo while/////
+
+let password;
+do
+{
+   password =window.prompt("enter pwd !!!!");
+
+}while(password=="")
+
+
+//////////////////  FOR LOOP//////////////////////
+for (let counter=1; counter<=10; counter+=1)
+{
+    console.log(counter)
+    if(counter === 8 ){break;} //can use break to stop the loop
+}
+
+for (let i=1 ; i<10; i+=1)
+{   if(i===5){continue;}  //continue is used to skip that iteration and continue the loop
+    console.log(i);
+
+ 
+}
+
+
+/////////////////FUNCTIONS//////////////////////////////////
+
+function sing(name)
+{   console.log(name+" this song is 4 U ")
+    console.log("never gonna give you up ");
+    console.log("never gonna let you down ");
+    console.log("never gonna run around and desert you 🎶  ");
+}//create the function
+let namee="ACE"
+sing(namee);//call the function
+
+
+
+//example with return
+let area;
+let width;
+let height;
+width=window.prompt("enter width");
+height=window.prompt("enter height ");
+function calculateArea(W,H)
+{
+    return H*W;
+}
+console.log(calculateArea(width,height));
+
+///////////////////////TERNARY OPERATOR (aka tHE condition ? expressionIfTrue : exprIfFalse )//////
+//so basically it's like an if statement but with a shorter/weird syntax
+let adult =checkage(21);
+function checkage(age)
+{
+return age>=18 ? true : false;
+//     condition ?  returnif true : return if false
+}
+console?.log(adult);
+
+
+//////////LET  VS   VAR/////////////////////////////////
+
+//variable scope = where the variable is accessible
+// let = variables are limited to block scope {}
+// var = variables are limited to a function(){}
+
+// a global variable is declared outside any function
+// DO NOT USE VAR WITH GLOBAL VARIABLES AS IT CHANGES BROWSER'S WINDOW PROPERTIES
+// we will use let  for now as var is only used on old browsers
+
+///////////////TEMPLATE LITERALS///////////////////////
+//template literals = delimited with ( `` aka altG+7 ) instead of "" or ''
+//they allow embedded variables and expressions ?
+//example since I  didn't understand wtf the last sentence was
+let usernameee="aymen";
+let items =3;
+let total=75;
+
+
+//console.log("hello",usernameee);
+//console.log("you have",items,"items in yo cart");
+//console.log("your total is ",total);
+//instead you could
+let text=`hello ${usernameee}<br>
+you have ${items} in your cart<br>
+your total is $${total}
+`;
+document.getElementById("templateliterals").innerHTML=text;
+// so basically their only use is when you have to output a long text with variables
+//it makes it a bit easier to type 🙃
+
+/////////////////////////format currency////////////////////////////////////////
+let mynum = 100;
+//options are not necessary
+mynum=mynum.toLocaleString("en-US",{style:"currency", currency:"USD"}); //==> $100.00
+mynum=mynum.toLocaleString(undefined,{style:"percent"});//==>10,000%
+mynum=mynum.toLocaleString(undefined,{style:"unit",unit:"celsius"});//==>100°C
+console.log(mynum);
+*/
+//////////////////////////////////
